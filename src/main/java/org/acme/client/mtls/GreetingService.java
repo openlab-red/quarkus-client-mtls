486dd0ecf4eed7ca65dc11f115d74f76860b6bc5
Mattia Mascia <mmascia@redhat.com>
2020-04-16 15:55:29 +0100

    Adding applicationscope

--- src/main/java/org/acme/client/mtls/GreetingService.java
+++ src/main/java/org/acme/client/mtls/GreetingService.java
@@ -2,12 +2,14 @@ package org.acme.client.mtls;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/")
+@ApplicationScoped
 @RegisterRestClient
 public interface GreetingService {
 
